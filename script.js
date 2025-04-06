227f079a4e03068f74dad40b434a98d3dd5f716f
Miaou79 <videauarthur@gmail.com>
2025-04-06 23:15:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('statistics-box').addEventListener('click', function() {
+    document.getElementById('table-container').classList.toggle('hidden');
+});
+
+document.getElementById('ai-referee-box').addEventListener('click', function(){
+    document.getElementById('image-container').classList.toggle('hidden');
+});
